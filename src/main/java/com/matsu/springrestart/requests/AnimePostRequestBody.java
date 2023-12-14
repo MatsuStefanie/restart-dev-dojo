5345cfc4eb33f4328e3fb3130cdf1bd6916c54ae
Stefanie Silva <matsustefanie@gmail.com>
2023-12-14 01:37:48 -0300

    Adicionando builder a classe

--- src/main/java/com/matsu/springrestart/requests/AnimePostRequestBody.java
+++ src/main/java/com/matsu/springrestart/requests/AnimePostRequestBody.java
@@ -1,9 +1,11 @@
 package com.matsu.springrestart.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
 
     @NotBlank(message = "The anime name cannot be empty or null")
